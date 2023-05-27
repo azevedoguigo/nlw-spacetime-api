eacaeeccbaf536efc2bdf4bf7c3f3e829d3feae1
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-05-27 04:03:14 -0300

    feat: file uploads.

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,17 @@ import jwt from '@fastify/jwt'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastfy()
 
 app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -22,6 +29,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
